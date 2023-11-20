8337a8d8a65b4d4ab70072abcf81e48188b18799
Mariusz Rojewski <rojewski@webkon.eu>
2023-11-20 09:43:35 +0100

    cart 01

---
+++ src/context.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import reducer from "./reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
+import cartItems from "./data";
+
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: [...cartItems],
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return React.useContext(AppContext);
+};
